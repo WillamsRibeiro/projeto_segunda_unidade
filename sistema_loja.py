93007db7c749e23e7a9ab9ac4e183b0281688366
WillamsRibeiro <willamsribeiro1987@gmail.com>
2024-10-02 04:06:04 -0300

    msg

--- sistema_loja.py
+++ sistema_loja.py
@@ -3,7 +3,7 @@ banco_de_dados = {}
 
 
 def exibir():
-    print('1- cadastrar produtos\n2- editar \n3- relatorio\n4- excluir')
+    print('1- cadastrar produtos\n2- editar preço \n3- relatorio\n4- excluir')
 
 def opcoes_do_programa():
     exibir()
@@ -19,64 +19,69 @@ def opcoes_do_programa():
             tempo = input('ano de vencimento: ')
             preco = float(input('digite o preço do produto: '))
             banco_de_dados[cod]= {'cod':cod , 'nome':cosmetico , 'categoria': tipo, 'val': validade, 'ano': tempo, 'venda': preco}
-            print(f" código do produto: {banco_de_dados[cod]['cod']} | nome: {banco_de_dados[cod]['nome']} | categoria: {banco_de_dados[cod]['categoria']} | vencimento: {banco_de_dados[cod]['val']} | ano {banco_de_dados[cod]['ano']} faturamento{banco_de_dados[cod]['venda']}")
+            print(f" código do produto:  nome: {banco_de_dados[cod]['nome']} | categoria: {banco_de_dados[cod]['categoria']} | vencimento: {banco_de_dados[cod]['val']} | ano {banco_de_dados[cod]['ano']} | faturamento {banco_de_dados[cod]['venda']}")
             input('pressione uma tecla para retornar')
             opcoes_do_programa()
 
 
         elif opcao == 2:
-            pass
-
+            editar()
+            input('pressione uma tecla para retornar ')
+            opcoes_do_programa()
 
 
         elif opcao ==3:
+            print()
             relatorio()
-            print('1- cadastrar cliente\n2- para cadastrar produtos\n3- relatorio de vendas\n4- editar cliente:')
-            int(input(''))
+
+            int(input())
+            opcoes_do_programa()
 
         elif opcao ==4:
-            produtos()
+            excluir()
+            input('pressione uma tecla para retornar ')
+            opcoes_do_programa()
+
 
     else:
-        pass
+
         opcoes_do_programa()
 def relatorio():
 
-    produto = input('digite o nome do produto\n ')
 
-    if produto in banco_de_dados.values():
-        print(banco_de_dados[produto])
-        input('pressione uma tecla para retornar')
+    produto = input('digite o código do produto\n ')
+
+    if produto in banco_de_dados:
+            print(banco_de_dados[produto])
+            input('pressione uma tecla para retornar ')
+            opcoes_do_programa()
     else:
         print('produto não cadastrado')
-        input('pressione uma tecla para retornar')
+        input('pressione uma tecla para retornar ')
+        opcoes_do_programa()
+
 
 def editar():
-   pass
-def produtos():
-    nome_do_produto = input('digite o nome do produto ')
+    print(banco_de_dados)
+    cod=input('codigo do produto ')
+    if cod in banco_de_dados:
+        banco_de_dados['venda'] = input('digite o novo preço')
+        print(f'{banco_de_dados}')
 
-    if nome_do_produto in mercadorias:
-        print(f' nome: {mercadorias[nome_do_produto]["nome"] }')
-    else:
-        print('cliente não cadastrado')
-        input('pressione uma tecla para retornar')
 
 
 def excluir():
-    item = input('digite o nome do item que deseja excluir: ')
-    mercadorias[nome]= item
-    if item in mercadorias:
-        del mercadorias[item]
-        print('produto excluído com sucesso')
-
-
-
-
-
-
-
-
+    print (banco_de_dados)
+    cod = input('digite o código do produto que deseja excluir\n')
+    print(f' tem certeza que deseja excluir o produto? {cod}')
+    if cod in banco_de_dados:
+        del banco_de_dados[cod]
+        print(f'produto {cod} excluído com sucesso!')
+        print(f"banco de dados atualizado {banco_de_dados}")
+    else:
+        print('produto não encontrado')
+    print('banco de dados atualizado ',banco_de_dados)
+    opcoes_do_programa()
 
 
 
@@ -84,7 +89,6 @@ def main():
 
     opcoes_do_programa()
     relatorio()
-    produtos()
     editar()
     excluir()
 if __name__ == '__main__':
